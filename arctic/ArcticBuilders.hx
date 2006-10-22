37bc6cd6a781a04062eac10f69cb1e4fb1a294fb
muthunivas <muthunivas@74b7a136-1220-0410-81ce-1d32f1f57809>
2006-10-22 20:33:22 +0000

    - Modified TextInput interface to make it independent of Flash types.  Supported for Flash9 also. - Implemented RadioButtonGroup in ArcticBuilders. In Flash9 selection does not work properly because of the known issue with SelectList .  ToggleButtonGroup arcticblock is removed.

--- arctic/ArcticBuilders.hx
+++ arctic/ArcticBuilders.hx
@@ -12,4 +12,31 @@ class ArcticBuilders {
 				+ "<br><p align='center'><b>" + day + "</b></font>";
 		return Background(0x000000, Border(1, 1, Background(0xFFFCA9, ConstrainWidth(75, 75, ConstrainHeight(75, 75, Text(text))))));
 	}
+	
+	static public function makeRadioButtonGroup(texts : Array<String>, onSelect : Int -> Void) : ArcticBlock {
+		var stateChooser = [];
+		var onInit = function (setState) { 
+			stateChooser.push(setState); 
+			// Select the first entry by default
+			if (1 == stateChooser.length) {
+				stateChooser[0](true);
+			}
+		};
+		var toggleButtons : Array<ArcticBlock> = [];
+		for (text in texts) {
+			// TODO : Draw standard radio button graphics instead of using 'O' and 'Ø'
+			var selected   = ColumnStack([Border(1, 1, Text("<font color='#000000' face='arial' size='8'><b>Ø</b></font>")),
+									      Border(1, 1, Text(text))]);
+			var unselected = ColumnStack([Border(1, 1, Text("<font color='#000000' face='arial' size='8'><b>O</b></font>")),
+									      Border(1, 1, Text(text))]);
+			toggleButtons.push(ToggleButton(selected, unselected, false, null, onInit));
+		}
+		var onSelectHandler = function (index : Int) {
+			for (i in 0...stateChooser.length) {
+				stateChooser[i](i == index);
+			}
+			onSelect(index);
+		}
+		return SelectList(toggleButtons, onSelectHandler);
+	}
 }
