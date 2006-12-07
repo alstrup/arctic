106f9e95ca67dfe3f86a41f954e27843d3986a3f
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2006-12-07 08:46:34 +0000

    - Center time stamp text

--- trunk/arctic/Arctic.hx
+++ trunk/arctic/Arctic.hx
@@ -27,7 +27,7 @@ class Arctic {
 		var text = "<font color='#000000' face='arial'><p align='center'><b>" + months[date.getMonth()]
 				+ "</b><br><p align='center'><b><font size='32'>" + date.getDate() + "</font></b>"
 				+ "<br><p align='center'><b>" + day + "</b></font>";
-		return Background(0x000000, Border(1, 1, Background(0xFFFCA9, ConstrainWidth(75, 75, ConstrainHeight(75, 75, Text(text))))));
+		return Background(0x000000, Border(1, 1, Background(0xFFFCA9, ConstrainWidth(75, 75, ConstrainHeight(75, 75, ColumnStack([Filler,Text(text), Filler]))))));
 	}
 	
 	/**
