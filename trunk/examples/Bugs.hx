7c54b4df334946aa911925310b6a9c86f2500915
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2007-01-17 12:41:04 +0000

    - Do not add a scrollbar is there is not enough room (which often there is not when we calculate the minimum metrics) - Add a bug to the test suite which was triggered by the above problem

--- trunk/examples/Bugs.hx
+++ trunk/examples/Bugs.hx
@@ -20,7 +20,7 @@ class Bugs {
 		// To make a screen, first build the data structure representing the contents
 		var me = this;
 		var screen;
-		
+		var fullsize = true;
 		switch (count) {
 			case 0:
 			// Tooltips were underneath following elements
@@ -29,7 +29,7 @@ class Bugs {
 				ColumnStack( [
 					Arctic.makeTooltip( Text("This text has a tooltip."), "This tooltip should come on top of everything"),
 					Text("Don't go beneath this stuff"),
-					Arctic.makeSimpleButton( "Next bug", function() { me.next(); } )
+					Arctic.makeSimpleButton( "Next bug", next )
 				] ) );
 			case 1:
 			// Not possible to select text in textinput nested in dragable
@@ -37,37 +37,43 @@ class Bugs {
 				LineStack( [
 					Arctic.makeDragable(true, true, true, Background(0x8080ff, Border(10, 10, TextInput("Selection with mouse should work", 200, 20)))),
 					Arctic.makeDragable(true, true, true, Background(0x8080ff, Border(10, 10, TextInput("Selection with mouse should work", 200, 20)))),
-					Arctic.makeSimpleButton( "Next bug", function() { me.next(); } )
+					Arctic.makeSimpleButton( "Next bug", next )
 				]);
 			case 2:
 			// Nested dragables were dually dragged
 			screen = 
 				LineStack( [
 					Arctic.makeDragable(true, true, true, ConstrainWidth(300, 300, ConstrainHeight(100, 100, Background(0x8080ff, Border(10, 10, Arctic.makeDragable(true, true, true, Background(0x80ff80, Border(10, 10, TextInput("Selection with mouse should work", 200, 20))))))))),
-					Arctic.makeSimpleButton( "Next bug", function() { me.next(); } )
+					Arctic.makeSimpleButton( "Next bug", next )
 				]);
 			case 3:
 			// Nested LineStack become too big:
 			screen = Border(0, 120, Background(0xf08000, 
 				LineStack( [ 
 					LineStack( [ Arctic.makeText("Text", 100), Filler ] ),
-					Arctic.makeSimpleButton( "Next bug", function() { me.next(); } )
+					Arctic.makeSimpleButton( "Next bug", next )
 				] )));
 			case 4:
 			// Text radio-choice does not work
 			screen = 
 					LineStack( [ 
 						Arctic.makeTextChoice([ "See custom block", "See dragable blocks" ], function(i : Int, text : String) { }, 0, 20).block,
-						Arctic.makeSimpleButton( "Next bug", function() { me.next(); } )
+						Arctic.makeSimpleButton( "Next bug", next )
 					]);
-
+			case 5:
+			// This should be a 20x20 red box
+			screen = LineStack( [ Background(0xff0000, ColumnStack( [ Button(Fixed(20, 20), Fixed(20, 20), next), Filler ] )) ] );
+			fullsize = false;
 			default:
 			screen = Text("The end");
 		}
 		 // Then construct the arctic object
 		arcticView = new ArcticView( screen );
+		if (!fullsize) {
+			arcticView.adjustToFit(parent, 0, 0);
+		}
 		// And finally display on the given movieclip
-		var root = arcticView.display(parent, true);
+		var root = arcticView.display(parent, fullsize);
 		++count;
 	}
 	public var arcticView : ArcticView;
