3687b59f9633e355c2a518a1466de351d19865a2
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2007-03-15 08:44:24 +0000

    - Simplify making a new release: Just use makeZip

--- makeZip.bat
+++ makeZip.bat
@@ -1,6 +1,13 @@
 del arctic.zip
 
+cd examples
+haxe DevArcticExamples.hxml
+haxe Bugs.hxml
+cd ..
+
 SET ZIP="c:\program files\7-zip\7z.exe"
 IF NOT EXIST %ZIP% SET ZIP="c:\programmer\7-zip\7z.exe"
 
-%ZIP% a -tzip -r -x!.svn -x!launch*.bat -x!makeZip.bat arctic.zip *.*
+%ZIP% a -tzip -r -x!.svn -x!auto*.bat -x!makeZip.bat arctic.zip *.*
+
+haxelib submit arctic.zip
