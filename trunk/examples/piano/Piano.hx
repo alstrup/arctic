9311715d27fb6da7b9671d7efdc2b50cd09699af
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2007-05-12 20:42:44 +0000

    - Keys on the piano

--- trunk/examples/piano/Piano.hx
+++ trunk/examples/piano/Piano.hx
@@ -9,6 +9,42 @@ class Piano {
 	}
 	
 	public function new() {
+		
+		// c#: 10,0 -> 35,90
+		// d#: 35,0 -> 64,90
+		
+		var me = this;
+		var blackkey = function(note) {
+			return Button(Background(0x808080, Fixed(25, 78), 0), Background(0xffffff, Fixed(25, 78), 50),
+					callback(me.press, note));
+		}
+		var key = function(note, width) {
+			return Button(Background(0x808080, Fixed(width, 45), 0), Background(0x000000, Fixed(width, 45), 50),
+					callback(me.press, note));
+		}
+		var keys =
+			LineStack( [
+				Fixed(0, 5),
+				ColumnStack( [ 
+					Fixed(15, 0),
+					blackkey("c#"),
+					blackkey("d#"),
+					Fixed(8, 0),
+					blackkey("f#"),
+					blackkey("g#"),
+					blackkey("a#"),
+				]),
+				ColumnStack( [
+					key("c", 25),
+					key("d", 23),
+					key("e", 23),
+					key("f", 22),
+					key("g", 21),
+					key("a", 23),
+					key("h", 23)
+				])
+			]);
+		
 		var ui = 
 			LineStack( [
 				Fixed(0, 10),
@@ -18,13 +54,20 @@ class Piano {
 				Fixed(0, 10),
 				Background( 0x1C1B1C, 
 					Border(20, 2,
-						Picture("keys.swf", 0.2 * 800.0, 0.2 * 640.0, 1.0)
+						OnTop(
+							Picture("keys.swf", 0.2 * 800.0, 0.2 * 640.0, 1.0),
+							keys
+						)
 					)
 				)
 			]);
 		
 		view = new ArcticView(ui, flash.Lib.current);
 		view.display(true);
+	}
+	
+	private function press(k) {
+		trace(k);
 	}
 	
 	private var view : ArcticView;
