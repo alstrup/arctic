28ff7f5f7ae469d2588ef45ada2e008ea95cc818
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2007-08-09 18:00:33 +0000

    - Ignore all .bat files when packaging

--- makeZip.bat
+++ makeZip.bat
@@ -8,6 +8,6 @@ cd ..
 SET ZIP="c:\program files\7-zip\7z.exe"
 IF NOT EXIST %ZIP% SET ZIP="c:\programmer\7-zip\7z.exe"
 
-%ZIP% a -tzip -r -x!.svn -x!launch*.bat -x!makeZip.bat arctic.zip *.*
+%ZIP% a -tzip -r -x!.svn -x!*.bat -x!makeZip.bat arctic.zip *.*
 
 haxelib submit arctic.zip
