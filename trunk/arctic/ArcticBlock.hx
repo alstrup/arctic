d68674bf1453c325a7db431617b6acd0f952582a
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2007-12-17 11:46:51 +0000

    - Extend Scale block with facility to not grow large than a given scale

--- trunk/arctic/ArcticBlock.hx
+++ trunk/arctic/ArcticBlock.hx
@@ -251,7 +251,7 @@ enum ArcticBlock {
 	/**
 	 * Scale a block - original size is 1.0
 	 */
-	Scale(block : ArcticBlock);
+	Scale(block : ArcticBlock, ? maxScale : Float);
 	
 	/// Special block which is useful for debugging
 	DebugBlock(id : String, block : ArcticBlock);
