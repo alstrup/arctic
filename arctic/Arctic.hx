33bff0a57ad2db7991eba6bef9e1194f14a50e11
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2008-02-15 22:00:02 +0000

    - Support for baseurl in arctic

--- arctic/Arctic.hx
+++ arctic/Arctic.hx
@@ -17,6 +17,9 @@ class Arctic {
 	static public var textSharpness : Null<Float> = null;
 	static public var textGridFit = 0;
 
+	/// For redirecting all net requests
+	static public var baseurl = "";
+
 	/// Make a text using the given parameters. Default color is black. If font is omitted, the default font is used
 	static public function makeText(text : String, ?size : Float, ?color : String, ?font : String, ?isEmbedded : Bool, ?wordWrap : Null<Bool>, ?selectable: Null<Bool>) {
 		return Text(wrapWithDefaultFont(text, size, color, font), if (isEmbedded == null) isDefaultFontEmbedded else isEmbedded, wordWrap, selectable);
