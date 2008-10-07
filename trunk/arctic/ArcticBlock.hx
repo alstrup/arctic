d9d4b2deab1dec00777a0f881a2e327d2e3a1691
asgeralstrup <asgeralstrup@74b7a136-1220-0410-81ce-1d32f1f57809>
2008-10-07 07:23:16 +0000

    - Details about formatting

--- trunk/arctic/ArcticBlock.hx
+++ trunk/arctic/ArcticBlock.hx
@@ -280,10 +280,12 @@ enum ArcticBlock {
 	 * Scale a block - original size is 1.0. The scaling can be constrained to maxScale.
 	 */
 	Scale(block : ArcticBlock, ? maxScale : Float);
+	
 	#if flash9
+	/// Rotates a block. The angle is in degrees
 	Rotate(block : ArcticBlock, ?angle : Float);
 	#end
-	
+
 	/// Special block which is useful for debugging
 	DebugBlock(id : String, block : ArcticBlock);
 }
