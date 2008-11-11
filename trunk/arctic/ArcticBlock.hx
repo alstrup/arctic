cc216751f13d4208c366a4a0b2f06467150e5fbe
michael.pliskin <michael.pliskin@74b7a136-1220-0410-81ce-1d32f1f57809>
2008-11-11 23:47:16 +0000

    - sprite-based arctic seems to work. Needs testing though, but seems faster

--- trunk/arctic/ArcticBlock.hx
+++ trunk/arctic/ArcticBlock.hx
@@ -2,7 +2,31 @@ package arctic;
 
 // We introduce an alias for MovieClip which works in both Flash 8 & 9. See also ArcticMC.hx
 #if flash9
-	typedef ArcticMovieClip = flash.display.MovieClip;
+	//typedef ArcticMovieClip = flash.display.MovieClip;
+	class ArcticMovieClip extends flash.display.Sprite {
+		private var hash: Hash<Dynamic>;
+
+		public function new() {
+			super();
+			hash = new Hash();
+		}
+
+		public inline function set(name: String, value: Dynamic) {
+			hash.set(name, value);
+		}
+
+		public inline function get(name): Dynamic {
+			return hash.get(name);
+		}
+
+		public inline function has(name): Bool {
+			return hash.exists(name);
+		}
+
+		public inline function remove(name) {
+			hash.remove(name);
+		}
+	}
 #else flash
 	typedef ArcticMovieClip = flash.MovieClip
 #else neko
