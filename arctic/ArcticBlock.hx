9cf245498afb1f1fddc175a69107383824657e4b
victor.polozov <victor.polozov@74b7a136-1220-0410-81ce-1d32f1f57809>
2009-09-11 18:22:17 +0000

    Changed VerticalAlignment enum to Float [0..1]

--- arctic/ArcticBlock.hx
+++ arctic/ArcticBlock.hx
@@ -168,9 +168,9 @@ enum ArcticBlock {
 	* 			free space
 	* If maxWidth is null, the block will grow and use all available width.
 	* lowerWidth should be in [0 ... 1] range. By default its value is 0.45
-	* verticalAlignment and defines blocks alignment in the row. Could be: Top, Bottom, Center
+	* verticalAlignment defines blocks alignment in the row. [Top ... Bottom] corresponds to [0 ... 1] range.
 	*/
-	Wrap(blocks: Array<ArcticBlock>, ?maxWidth: Float, ?xspacing: Null<Float>, ?yspacing: Null<Float>, ?eolFiller: ArcticBlock, ?lowerWidth : FloatWrap(blocks: Array<ArcticBlock>, ?maxWidth: Float, ?xspacing: Null<Float>, ?yspacing: Null<Float>, ?eolFiller: ArcticBlock, ?lowerWidth : Float, ?verticalAlignment : VerticalAlignment ););
+	Wrap(blocks: Array<ArcticBlock>, ?maxWidth: Float, ?xspacing: Null<Float>, ?yspacing: Null<Float>, ?eolFiller: ArcticBlock, ?lowerWidth : Float, ?verticalAlignment : Float );
 
 	/// Add a scrollbar if necessary
     ScrollBar(block : ArcticBlock);
@@ -544,10 +544,4 @@ enum AnimateComponent {
 	ScaleX( f : Float -> Float);
 	ScaleY( f : Float -> Float);
 	Rotation( f : Float -> Float);
-}
-
-enum VerticalAlignment {
-	Top;
-	Bottom;
-	Center;
 }
